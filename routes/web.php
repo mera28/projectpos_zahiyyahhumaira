abb2eecfd1340314cca196662f7d120751a596e2
Zahiyyah Humaira <@zahiyyahhumaira.com>
2024-04-19 10:06:56 +0700

    bab 4

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,5 @@ Route::resource('kategori',CategoryController::class)
 ->except('show','destroy','create');
 
 Route::resource('pelanggan',CustomerController::class)->except('destroy');
+
+Route::resource('produk',ProductController::class);
